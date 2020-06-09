44ef58e64eb1a5b989486856002151c417ffec00
Lawrence Chen <50194219+lawrencechen0921@users.noreply.github.com>
2020-06-09 16:48:09 +0800

    variable port

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.get('/', async function (req, res) {
 
 });
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 app.listen(port,() => {
     console.log("smartfridge");
 });
