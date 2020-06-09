8bcfc379f487f374241141237e5ef0924755b097
Lawrence Chen <50194219+lawrencechen0921@users.noreply.github.com>
2020-06-09 15:47:10 +0800

    variable port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req, res) =>{
+app.get('/',(req, res) => {
     res.send("Welcome to the smart fridge")
 });
 
-app.listen(3000,() =>{
+const port = process.env.port || 3000;
+app.listen(port,() => {
     console.log("Smartfridgeapp");
 });
+
